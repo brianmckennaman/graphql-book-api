7aa3c11b08c2214417a3e4bc8be60c8c8c7944d7
Brian McKenna <brianmckennaman@gmail.com>
2022-11-28 22:14:38 -0500

    added cloned starter code

---
+++ client/build/precache-manifest.5185099a86a9e680725444000365ac14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c233589c2c30aabdd9d663c3659fcc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c26404c69d86e723b9c",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "9e89fe86d911030ed407",
+    "url": "/static/css/main.d137a5ff.chunk.css"
+  },
+  {
+    "revision": "7c26404c69d86e723b9c",
+    "url": "/static/js/2.1797b3e0.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.1797b3e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e89fe86d911030ed407",
+    "url": "/static/js/main.4a1e7859.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.4a1e7859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
